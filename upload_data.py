4db0ca8303ac63ad8b47f7dd9d39f4953f0ea2dd
SmellyCat <1282399562@qq.com>
2019-07-04 15:20:36 +0800

    更新 使用并发函数

--- upload_data.py
+++ upload_data.py
@@ -1,7 +1,9 @@
 ﻿# -*- coding: utf-8 -*-
 import time
 import json
+import math
 import socket
+from concurrent.futures import ProcessPoolExecutor as Pool
 
 import arrow
 
@@ -25,84 +27,74 @@ class UploadData(object):
         self.kc = None
         self.uk = None
         self.con = ConsulAPI()
-        #self.con.path = dict(self.my_ini['consul'])['path']
-
-        self.item = None
-        self.part_list = list(range(60))
 
         self.local_ip = socket.gethostbyname(socket.gethostname())  # 本地IP
 
-    def upload_data(self):
+        self.part_list = list(range(60))
+
+        self.workers = dict(self.my_ini['sys'])['workers']
+
+        self.weight = dict(self.my_ini['sys'])['weight']
+        self.last_items = 0
+        self.lost_data = []
+
+    def upload_data(self, weight):
         items = []
         offsets = {}
-        for i in range(400):
+        pool = Pool(max_workers=self.workers)
+        for i in range(weight*100):
             msg = self.kc.c.poll(0.005)
-            #print(msg.value())       
+        
             if msg is None:
                 continue
             if msg.error():
                 continue
             else:
                 m = json.loads(msg.value().decode('utf-8'))['message']
-                #print(m['kkdd_id'])
-                if m['kkdd_id'] is None:
-                    continue
-                if len(m['kkdd_id']) < 9:
-                    continue
-                if m['kkdd_id'][:6] in ('441324', '441303') or m['kkdd_id'] in ('441302030', '441302031', '441302032', '441302033', '441302034', '441302035', '441302036', '441302037', '441302045', '441302046', '441302047'):
-                    m['img_path'] = m['imgurl']
-                    m['fxbh'] = m['fxbh_id']
-                    items.append(m)
+                m['img_path'] = m['imgurl']
+                m['fxbh'] = m['fxbh_id']
+                items.append(m)
             par = msg.partition()
             off = msg.offset()
             offsets[par] = off
-        #print(offsets)
         if offsets == {}:
             return 0
         else:
+            #print('items={0}'.format(len(items)))
+            #logger.info('items={0}'.format(len(items)))
             if len(items) > 0:
-                #logger.info(items)
-                print('{0}, items={1}'.format(items[-1]['jgsj'], len(items)))
-                self.uk.post_kakou(items)                 # 上传数据
+                results = []
+                z = math.ceil(float(len(items))/100)
+                for i in range(z):
+                    data = items[i*100:(i+1)*100]
+                    obj = pool.submit(self.uk.post_data, data)
+                    results.append(obj)
+                pool.shutdown()
+                result = [obj.result().status_code for obj in results]
+                print(result)
+                logger.info(result)
+                info = '{0}, items={1}'.format(items[-1]['jgsj'], len(items))
+                print(info)
+                logger.info(info)
+                #self.uk.post_kakou(items)                 # 上传数据
             self.kc.c.commit(async=False)
-            info_msg = 'items={0}, offset={1}'.format(len(items), offsets)
-            #print(info_msg)
-            logger.info(info_msg)
-            return 1
+            #print(offsets)
+            logger.info(offsets)
+            return len(items)
 
     def main_loop(self):
-        count = 0
         while 1:
             try:
-                #if not self.get_lock():
-                #    if self.kc is not None:
-                #        del self.kc
-                #        self.kc = None
-                #    self.item = None
-                #    self.part_list = []
-                #    time.sleep(2)
-                #    continue
-                #print('test')
-                if count > 3:
-                    logger.info('exit')
-                    exit()
                 if self.kc is None:
                     self.kc = KafkaConsumer(**dict(self.my_ini['kafka']))
                     self.kc.assign(self.part_list)
                 if self.uk is not None and self.uk.status:
-                    n = self.upload_data()
-                    if n > 0:
-                        count = 0
-                    else:
-                        count += 1
-                        logger.info('count={0}'.format(count))
-                        time.sleep(1)
+                    self.upload_data(self.weight)
                 else:
                     self.uk = UnionUpload(**dict(self.my_ini['union']))
                     self.uk.status = True
             except Exception as e:
                 logger.exception(e)
-                count += 2
-                logger.info('count={0}'.format(count))
                 time.sleep(15)
+
         
---
+++ upload_data1.py
@@ -0,0 +1,108 @@
+﻿# -*- coding: utf-8 -*-
+import time
+import json
+import socket
+
+import arrow
+
+from helper_consul import ConsulAPI
+from helper_kafka_consumer import KafkaConsumer
+from helper_union_upload_v2 import UnionUpload
+from my_yaml import MyYAML
+from my_logger import *
+
+
+debug_logging('/home/logs/error.log')
+logger = logging.getLogger('root')
+
+
+class UploadData(object):
+    def __init__(self):
+        # 配置文件
+        self.my_ini = MyYAML('/home/my.yaml').get_ini()
+
+        # request方法类
+        self.kc = None
+        self.uk = None
+        self.con = ConsulAPI()
+        #self.con.path = dict(self.my_ini['consul'])['path']
+
+        self.item = None
+        self.part_list = list(range(60))
+
+        self.local_ip = socket.gethostbyname(socket.gethostname())  # 本地IP
+
+    def upload_data(self):
+        items = []
+        offsets = {}
+        for i in range(400):
+            msg = self.kc.c.poll(0.005)
+            #print(msg.value())       
+            if msg is None:
+                continue
+            if msg.error():
+                continue
+            else:
+                m = json.loads(msg.value().decode('utf-8'))['message']
+                #print(m['kkdd_id'])
+                if m['kkdd_id'] is None:
+                    continue
+                if len(m['kkdd_id']) < 9:
+                    continue
+                if m['kkdd_id'][:6] in ('441324', '441303') or m['kkdd_id'] in ('441302030', '441302031', '441302032', '441302033', '441302034', '441302035', '441302036', '441302037', '441302045', '441302046', '441302047'):
+                    m['img_path'] = m['imgurl']
+                    m['fxbh'] = m['fxbh_id']
+                    items.append(m)
+            par = msg.partition()
+            off = msg.offset()
+            offsets[par] = off
+        #print(offsets)
+        if offsets == {}:
+            return 0
+        else:
+            if len(items) > 0:
+                #logger.info(items)
+                print('{0}, items={1}'.format(items[-1]['jgsj'], len(items)))
+                self.uk.post_kakou(items)                 # 上传数据
+            self.kc.c.commit(async=False)
+            info_msg = 'items={0}, offset={1}'.format(len(items), offsets)
+            #print(info_msg)
+            logger.info(info_msg)
+            return 1
+
+    def main_loop(self):
+        count = 0
+        while 1:
+            try:
+                #if not self.get_lock():
+                #    if self.kc is not None:
+                #        del self.kc
+                #        self.kc = None
+                #    self.item = None
+                #    self.part_list = []
+                #    time.sleep(2)
+                #    continue
+                #print('test')
+                if count > 3:
+                    logger.info('exit')
+                    exit()
+                if self.kc is None:
+                    self.kc = KafkaConsumer(**dict(self.my_ini['kafka']))
+                    self.kc.assign(self.part_list)
+                if self.uk is not None and self.uk.status:
+                    n = self.upload_data()
+                    if n > 0:
+                        count = 0
+                    else:
+                        count += 1
+                        logger.info('count={0}'.format(count))
+                        time.sleep(1)
+                else:
+                    self.uk = UnionUpload(**dict(self.my_ini['union']))
+                    self.uk.status = True
+            except Exception as e:
+                logger.exception(e)
+                count += 2
+                logger.info('count={0}'.format(count))
+                time.sleep(15)
+        
